da0e41ed0405f2b496f2684a0342b79a9c32c984
samuel <samuellyworld@gmail.com>
2021-10-08 12:32:20 -0700

    fixed  bugs

--- preloader.js
+++ preloader.js
@@ -1,24 +1,46 @@
 const preloaderDiv = document.getElementById('preloader')
 const preloaderSpinner = document.querySelector('.preloader-spinner');
 const preloaderTextContent = document.querySelector('.text-content');
+const mainContent = document.getElementById('main-content');
+
+
+let preloaderIsPaused = false;
 
 const preloader = () => {
     preloaderDiv.hidden = false
     preloaderSpinner.hidden = false;
+    preloaderTextContent.hidden = true;
+    loadTextPreloader()
+   
+     
+}
+
+const loadTextPreloader =  () => {
+    mainContent.hidden = true;
+    preloaderTextContent.hidden = false;
     setTimeout(() => {
         preloaderSpinner.hidden = true;
-      preloaderTextContent.textContent = 'Frontend with React';
-      
-     
+      preloaderTextContent.textContent = 'Frontend with React 👉';  
     }, 1000);
     setTimeout(() => {
         preloaderSpinner.hidden = true
-      preloaderTextContent.textContent = 'Backend with NodeJs';
-    }, 3000);
+      preloaderTextContent.textContent = 'Backend with NodeJs 👈 ';
+    }, 2000);
     setTimeout(() => {
         preloaderSpinner.hidden = true
-      preloaderTextContent.textContent = 'Fullstack Developer';
-    }, 5000);
+      preloaderTextContent.textContent = 'Fullstack Developer 👉👈';
+    }, 3000);
 }
 
-// preloader()
+const mainContentPage =  () => {
+    preloader();
+ setTimeout(() => {
+       preloaderDiv.hidden = true;
+       mainContent.hidden = false;
+    }, 4000)
+ 
+    
+   
+}
+
+mainContentPage()
