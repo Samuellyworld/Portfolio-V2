ffb4c55bb60982ab43dc461795b99992470dce86
samuel <samuellyworld@gmail.com>
2021-10-10 03:54:34 -0700

    added animations to preloader text 🌴

--- preloader.js
+++ preloader.js
@@ -1,6 +1,7 @@
 const preloaderDiv = document.getElementById('preloader')
 const preloaderSpinner = document.querySelector('.preloader-spinner');
-const preloaderTextContent = document.querySelector('.text-content');
+// const preloaderTextContent = document.querySelector('.text-content');
+const preloaderTextContainer = document.querySelector('.preloader-text-container');
 const mainContent = document.getElementById('main-content');
 
 
@@ -10,28 +11,32 @@ let preloaderIsPaused = false;
 const preloader = () => {
     preloaderDiv.hidden = false
     preloaderSpinner.hidden = false;
-    preloaderTextContent.hidden = true;
+    // preloaderTextContent.hidden = true;
+    
+   setTimeout(() => {
     loadTextPreloader()
-   
+   }, 2000)
      
 }
 
 const loadTextPreloader =  () => {
     mainContent.hidden = true;
-    preloaderTextContent.hidden = false;
+    preloaderSpinner.hidden = true;
+    preloaderTextContainer.hidden = false;
     
-    setTimeout(() => {
-        preloaderSpinner.hidden = true;
-      preloaderTextContent.textContent = 'Frontend with React 🚀';  
-    }, 1000);
-    setTimeout(() => {
-        preloaderSpinner.hidden = true
-      preloaderTextContent.textContent = 'Backend with NodeJs 🕵🤳 ';
-    }, 2000);
-    setTimeout(() => {
-        preloaderSpinner.hidden = true
-      preloaderTextContent.textContent = 'Fullstack Developer 👉👈';
-    }, 3000);
+//     setTimeout(() => {
+//         preloaderSpinner.hidden = true;
+//       preloaderTextContent.textContent = 'Frontend with React 🚀';  
+//     }, 1000);
+//     setTimeout(() => {
+//         preloaderSpinner.hidden = true
+//       preloaderTextContent.textContent = 'Backend with NodeJs 🕵🤳 ';
+//     }, 2000);
+//     setTimeout(() => {
+//         preloaderSpinner.hidden = true
+//       preloaderTextContent.textContent = 'Fullstack Developer 👉👈';
+//     }, 3000);
+// 
 }
 
 const mainContentPage =  () => {
@@ -39,7 +44,7 @@ const mainContentPage =  () => {
  setTimeout(() => {
        preloaderDiv.hidden = true;
        mainContent.hidden = false;
-    }, 4000)
+    }, 12000)
  
     
    
