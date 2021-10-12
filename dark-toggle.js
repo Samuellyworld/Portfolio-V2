82a5dd9df202c7f1449b53961400d1c3e32c7619
samuel <samuellyworld@gmail.com>
2021-10-12 13:01:17 -0700

    adding paragraph

--- dark-toggle.js
+++ dark-toggle.js
@@ -1,16 +1,19 @@
 const toggleSwitch = document.querySelector('input[type="checkbox"]');
 const toggleIcon = document.getElementById('toggle-icon');
+const bigSamToggle = document.getElementById('big');
 
 function darkMode() {
 	toggleIcon.classList.remove('fa-sun');
 	// toggleIcon.children[1].classList.remove('ink')
 	toggleIcon.classList.add('fa-moon');
+	bigSamToggle.src = './anime.gif'
 }
 function lightMode() {
 	
 	toggleIcon.classList.remove('fa-moon')
 	toggleIcon.classList.add('fa-sun');
 	// toggleIcon.children[1].classList.add('ink');
+	 bigSamToggle.src = './sam.png'
 	}
 
 	// toggle theme dynamically 
