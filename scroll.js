8ed55102fa560ac311aeb15cbf39ba8162287db6
samuel <samuellyworld@gmail.com>
2021-10-13 07:57:33 -0700

    finished with scrollbar 🍿

--- scroll.js
+++ scroll.js
@@ -1,6 +1,7 @@
-const LocomotiveScroll = require('locomotive-scroll');
-
-const scroller = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-  smooth: true
- });
+// const scroll = new LocomotiveScroll({
+//       el: document.querySelector('[data-scroll-container]') ,
+//       smooth: true,
+//       reloadOnContextChange: true,
+//       multiplier: 0.65,
+//       inertia: 0.3,
+// });
