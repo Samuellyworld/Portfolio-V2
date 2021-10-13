06966d4198a81a04b8e03d51c0217aefde54d531
samuel <samuellyworld@gmail.com>
2021-10-13 16:33:49 -0700

    added AOS library cdns 🪐

--- preloader.js
+++ preloader.js
@@ -44,6 +44,12 @@ const mainContentPage =  () => {
  setTimeout(() => {
        preloaderDiv.hidden = true;
        mainContent.hidden = false;
+       AOS.init({
+        delay: 200, // values from 0 to 3000, with step 50ms
+        duration: 1000, // values from 0 to 3000, with step 50ms
+        once: false, // whether animation should happen only once - while scrolling down
+        mirror: false, // whether elements should animate out while scrolling past them
+        });
     }, 12000)
  
     
