6f8f3069e1a5974fa51c461511385cd489a59909
samuel <samuellyworld@gmail.com>
2021-11-04 04:12:48 -0700

    fixing preloader🌴

--- scroll.js
+++ scroll.js
@@ -1,7 +1,7 @@
-// const scroll = new LocomotiveScroll({
-//       el: document.querySelector('[data-scroll-container]') ,
-//       smooth: true,
-//       reloadOnContextChange: true,
-//       multiplier: 0.65,
-//       inertia: 0.3,
-// });
+const scroll = new LocomotiveScroll({
+      el: document.querySelector('[data-scroll-container]') ,
+      smooth: true,
+      reloadOnContextChange: true,
+      multiplier: 0.65,
+      inertia: 0.3,
+});
